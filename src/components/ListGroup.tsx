3fe253a2476724e63fb0b13fbd13e556d6b7ce42
Laurence A <laurencearthur14@gmail.com>
2023-11-15 11:51:05 +0530

    onClick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,18 @@
 function ListGroup() {
   let items = ["London", "Tokyo", "Paris", "New York", "Spain"]
-  items = []
+
   return (
     <>
       <h1>List</h1>
       {/* {items.length === 0 ? "No items found" : null} */}
-      {items.length === 0 && 'No items found'}
+      {items.length === 0 && "No items found"}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={() => console.log(item, index)}
+          >
             {item}
           </li>
         ))}
