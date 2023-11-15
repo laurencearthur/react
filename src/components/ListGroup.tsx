f376f96a1b70130573f256f686dcf7351bcdaedd
Laurence A <laurencearthur14@gmail.com>
2023-11-15 11:33:53 +0530

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
 function ListGroup() {
-  const items = ["London", "Tokyo", "Paris", "New York", "Spain"]
-
+  let items = ["London", "Tokyo", "Paris", "New York", "Spain"]
+  items = []
   return (
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? "No items found" : null} */}
+      {items.length === 0 && 'No items found'}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item} className="list-group-item">{item}</li>
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
         ))}
       </ul>
     </>
