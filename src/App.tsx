cca38b65d3692fc2ef99c1a9939884d06db9d2c7
laurencearthur <laurencearthur14@gmail.com>
2023-11-18 17:32:19 +0530

    react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 import Alert from "./components/Alert"
 import Button from "./components/Button"
+import { GoCopilot } from "react-icons/go"
 
 function App() {
   const [AlertMsg, setAlertMsg] = useState(false)
   return (
     <>
       {AlertMsg && <Alert onClose={() => setAlertMsg(false)}>my alert</Alert>}
+      <GoCopilot size={100} color="#BE3144" />
+      <br/>
       <Button color="danger" onClick={() => setAlertMsg(true)}>
         my button
       </Button>
