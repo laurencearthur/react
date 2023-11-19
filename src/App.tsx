6a32a60470237e0090107b6dce13d444eb1c2a2e
laurencearthur <laurencearthur14@gmail.com>
2023-11-19 17:44:31 +0530

    Building an ExpandableText Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from "react"
 import ExpandableText from "./components/ExpandableText"
 
-
 const App = () => {
-  
   return (
     <>
-      <ExpandableText >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi enim necessitatibus incidunt id in natus tempore corporis debitis commodi, doloremque aliquam asperiores est quae rem atque excepturi alias facere molestias consequuntur officiis libero voluptates, reprehenderit qui aspernatur! Dolore reiciendis modi molestias nam quos omnis cumque expedita iste beatae a, perspiciatis dignissimos, eius in, repellendus fugiat aut qui. Sunt saepe beatae accusantium, ea consequuntur enim excepturi odit eos dolorum quibusdam animi sapiente rerum tempora modi reprehenderit? Repellendus dicta error autem consectetur aliquam quo perferendis modi hic odit, praesentium a, itaque quisquam fugit obcaecati nemo vel cum consequuntur nam nisi expedita? Sint.</ExpandableText>
+      <ExpandableText>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi enim
+        necessitatibus incidunt id in natus tempore corporis debitis commodi,
+        doloremque aliquam asperiores est quae rem atque excepturi alias facere
+        molestias consequuntur officiis libero voluptates, reprehenderit qui
+        aspernatur! Dolore reiciendis modi molestias nam quos omnis cumque
+        expedita iste beatae a, perspiciatis dignissimos, eius in, repellendus
+        fugiat aut qui. Sunt saepe beatae accusantium, ea consequuntur enim
+        excepturi odit eos dolorum quibusdam animi sapiente rerum tempora modi
+        reprehenderit? Repellendus dicta error autem consectetur aliquam quo
+        perferendis modi hic odit, praesentium a, itaque quisquam fugit
+        obcaecati nemo vel cum consequuntur nam nisi expedita? Sint.
+      </ExpandableText>
     </>
   )
 }
