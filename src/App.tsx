9913adc347b7ee58fd7fcf335f5be08f149d1125
laurencearthur <laurencearthur14@gmail.com>
2023-11-19 10:34:08 +0530

    updating state ex-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { useState } from "react"
 
 const App = () => {
-  const [pizza, setPizza] = useState({
-    name: "spicy mac",
-    toppings: ["mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "product 1", quantity: 1 },
+      { id: 2, title: "product 2", quantity: 1 },
+    ],
   })
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "cheese"] })
-    console.log(pizza.toppings)
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1
+          ? { ...item, quantity: item.quantity + 1 }
+          : { ...item, quantity: 1 }
+      ),
+    })
+    console.log(cart.items.map((item) => item.quantity))
   }
 
   return (
