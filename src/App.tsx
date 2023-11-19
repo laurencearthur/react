92ae243e4fcbd626f1c0c2f5cc00f5f5cad54665
laurencearthur <laurencearthur14@gmail.com>
2023-11-19 22:08:23 +0530

    Building a form

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import ExpandableText from "./components/ExpandableText"
-
+import Form from "./components/Form"
 const App = () => {
   return (
     <>
-      <ExpandableText>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi enim
-        necessitatibus incidunt id in natus tempore corporis debitis commodi,
-        doloremque aliquam asperiores est quae rem atque excepturi alias facere
-        molestias consequuntur officiis libero voluptates, reprehenderit qui
-        aspernatur! Dolore reiciendis modi molestias nam quos omnis cumque
-        expedita iste beatae a, perspiciatis dignissimos, eius in, repellendus
-        fugiat aut qui. Sunt saepe beatae accusantium, ea consequuntur enim
-        excepturi odit eos dolorum quibusdam animi sapiente rerum tempora modi
-        reprehenderit? Repellendus dicta error autem consectetur aliquam quo
-        perferendis modi hic odit, praesentium a, itaque quisquam fugit
-        obcaecati nemo vel cum consequuntur nam nisi expedita? Sint.
-      </ExpandableText>
+      <Form />
     </>
   )
 }
