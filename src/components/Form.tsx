1b038cdfd8588b9e5a8ea3dc7ebc91027ba52d49
laurencearthur <laurencearthur14@gmail.com>
2023-11-20 22:52:35 +0530

    Disabling the Submit Button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -16,7 +16,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) })
 
   const onSubmit = (data: FormEvent) => console.log(data)
@@ -50,7 +50,7 @@ const Form = () => {
             <p className="text-danger">{errors.age.message}</p>
           )}
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button disabled={!isValid} type="submit" className="btn btn-primary">
           Submit
         </button>
       </form>
