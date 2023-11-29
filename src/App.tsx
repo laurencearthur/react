cabaa4b8269ec00d3329de8b4b15a2e16ee38cf9
laurencearthur <laurencearthur14@gmail.com>
2023-11-29 20:29:34 +0530

    Fetching Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { useState } from "react"
-import ProductList from "./components/ProductList"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
+interface User {
+  id: number
+  name: string
+}
 const App = () => {
-  const [category, setCategory] = useState('')
-
+  const [users, setUsers] = useState<User[]>([])
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data))
+  }, [])
   return (
-    <>
-      <select className="form-select" onChange={(event) => setCategory(event.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-     
-      <ProductList category={category}/>
-    </>
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
